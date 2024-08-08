0cfd7efab63e4c4689815c5c2bece22c7f4ef298
Alex Perry <alexperry@Alexs-MacBook-Pro-4.local>
2024-08-08 15:26:58 +0100

    fix tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
+  tsconfig: "./tsconfig.json",
   entry: ["src/index.ts", "src/Fields.ts", "src/Queries.ts", "src/Stages.ts", "src/Types.ts"],
   format: ["cjs", "esm"],
   dts: true,
