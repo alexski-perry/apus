5a3e21276c166cc4921d0c789a3391cd2258d2bb
Alex Perry <alexperry@Alexs-MacBook-Pro-4.local>
2024-08-08 15:29:07 +0100

    try fix lint issues

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  tsconfig: "./tsconfig.json",
   entry: ["src/index.ts", "src/Fields.ts", "src/Queries.ts", "src/Stages.ts", "src/Types.ts"],
   format: ["cjs", "esm"],
   dts: true,
