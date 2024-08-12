3a6218d4d2ddd0e3d165278558ea2678a8c202b5
Alex Perry <alexperry@Alexs-MacBook-Pro-4.local>
2024-08-12 20:38:36 +0100

    add toDate function

--- src/cypher/expression/function/date.ts
+++ src/cypher/expression/function/date.ts
@@ -1,4 +1,11 @@
 import { Date } from "@cypher/types/scalar/date";
+import { StringValue } from "@cypher/types/scalar/string";
 import { expression } from "@core/expression";
+import { DateTime } from "@cypher/types/scalar/date-time";
+import { LocalDateTime } from "@cypher/types/scalar/local-date-time";
 
 export const now = () => expression(Date)`now()`;
+
+export function toDate(date: Date | StringValue | DateTime | LocalDateTime) {
+  return expression(Date)`date(${date})`;
+}
