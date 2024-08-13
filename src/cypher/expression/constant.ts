dbb9d548801ceaa4acd4f79e2a11117adfe1de29
Alex Perry <alexperry@Alexs-MacBook-Pro-4.local>
2024-08-13 19:53:54 +0100

    add nullConst

--- src/cypher/expression/constant.ts
+++ src/cypher/expression/constant.ts
@@ -2,9 +2,15 @@ import { StringLiteral } from "@cypher/types/scalar/string";
 import { BooleanLiteral } from "@cypher/types/scalar/boolean";
 
 import { parameterize } from "@core/parameterize";
+import { expression } from "@core/expression";
+import { Null } from "@cypher/types/null";
 
 export const stringConst = <T extends string>(value: T): StringLiteral<T> =>
   parameterize(value, StringLiteral.makeType([value]));
 
 export const booleanConst = <T extends boolean>(value: T): BooleanLiteral<T> =>
   parameterize(value, BooleanLiteral.makeType(value));
+
+export function nullConst() {
+  return expression(Null)`null`;
+}
